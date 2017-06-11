b98345904c0b3aa8e2970a847a5af98254e8b21e
Black Viking <blackvkng@yandex.com>
2017-06-11 19:40:55 +0000

    viSQL Project

---
+++ installer.py
@@ -0,0 +1,28 @@
+import os
+
+content = """
+#!/bin/bash
+
+cd /opt/viSQL
+python2 viSQL.py "$@"
+"""
+
+def main():
+	if os.name != "nt":
+		if os.getuid() == 0:
+			os.system("git clone http://github.com/blackvkng/viSQL.git /opt/viSQL")
+			
+			file = open("/usr/bin/viSQL", "w")
+			file.write(content)
+			file.close()
+			
+			os.system("chmod +x /usr/bin/viSQL")
+
+			print "\n\n[+] Installation finished, type 'viSQL' to use program!"
+		else:
+			print "Run as root!"
+	else:
+		print "This script don't work on Windows!"
+
+if __name__ == "__main__":
+	main()
